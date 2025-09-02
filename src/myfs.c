eff47672bd4e46004115913731b9e1871b6553a2
YeswanthRam28 <yeswanthram.2007@gmail.com>
2025-09-02 11:57:35 +0530

    Add files via upload

---
+++ src/myfs.c
@@ -0,0 +1,83 @@
+#define FUSE_USE_VERSION 31
+#include <errno.h>
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+
+static const char *hello_str = "Hello World from FUSE!\n";
+static const char *hello_path = "/hello";
+
+static int myfs_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi)
+{
+    (void) fi;
+    memset(stbuf, 0, sizeof(struct stat));
+
+    if (strcmp(path, "/") == 0) {
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 2;
+    } else if (strcmp(path, hello_path) == 0) {
+        stbuf->st_mode = S_IFREG | 0444;
+        stbuf->st_nlink = 1;
+        stbuf->st_size = strlen(hello_str);
+    } else {
+        return -ENOENT;
+    }
+    return 0;
+}
+
+static int myfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+                        off_t offset, struct fuse_file_info *fi,
+                        enum fuse_readdir_flags flags)
+{
+    (void) offset; (void) fi; (void) flags;
+
+    if (strcmp(path, "/") != 0)
+        return -ENOENT;
+
+    filler(buf, ".", NULL, 0, 0);
+    filler(buf, "..", NULL, 0, 0);
+    filler(buf, hello_path + 1, NULL, 0, 0); // show "hello"
+    return 0;
+}
+
+static int myfs_open(const char *path, struct fuse_file_info *fi)
+{
+    if (strcmp(path, hello_path) != 0)
+        return -ENOENT;
+
+    if ((fi->flags & O_ACCMODE) != O_RDONLY)
+        return -EACCES;
+
+    return 0;
+}
+
+static int myfs_read(const char *path, char *buf, size_t size, off_t offset,
+                     struct fuse_file_info *fi)
+{
+    size_t len;
+    (void) fi;
+    if(strcmp(path, hello_path) != 0)
+        return -ENOENT;
+
+    len = strlen(hello_str);
+    if (offset < len) {
+        if (offset + size > len)
+            size = len - offset;
+        memcpy(buf, hello_str + offset, size);
+    } else
+        size = 0;
+
+    return size;
+}
+
+static const struct fuse_operations myfs_oper = {
+    .getattr    = myfs_getattr,
+    .readdir    = myfs_readdir,
+    .open       = myfs_open,
+    .read       = myfs_read,
+};
+
+int main(int argc, char *argv[])
+{
+    return fuse_main(argc, argv, &myfs_oper, NULL);
+}
